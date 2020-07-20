c4b81bfa7ef1abf06db9d4e6eb6ad9c1ea2d6e2b
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-20 20:49:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,10 +187,12 @@ end
 
 def big_shoe_rebounds
   largest_shoe = 0
+  largest_shoe_name = ""
   game_hash.each do |location, category|
     game_hash[location][:players].each_index do [player_index]
       if game_hash[location][:players][player_index][:shoe] > largest_shoe
         largest_shoe = game_hash[location][:players][player_index][:shoe]
+        largest_shoe_name = game_hash[location][:players][player_index][:player_name]
       end
     end
   end
